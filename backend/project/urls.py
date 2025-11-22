42a9a4cbd9abe652309118f5a08553b7bfc9e566
justforit4102 <deeppakhare19@gmail.com>
2025-11-22 20:52:01 +0530

    deploying backend to frontend and applying token authentication

--- backend/project/urls.py
+++ backend/project/urls.py
@@ -4,12 +4,17 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from rest_framework.authtoken.views import obtain_auth_token
+from django.views.static import serve
+from pathlib import Path
+
+PROJECT_ROOT = Path(__file__).resolve().parents[1]  # adjust as needed
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("api.urls")),
     path("api-auth/", include("rest_framework.urls")),# browsable login for session auth
     path("api-token-auth/", obtain_auth_token),
+    path("samples/<path:filename>", lambda request, filename: serve(request, filename, document_root=str(PROJECT_ROOT / "samples"))),
 ]
 
 # serve media in dev
